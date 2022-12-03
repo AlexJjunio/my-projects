7f6ea6eedf8070befa8f828f9a8928dc0b6bbade
Mayk Brito <Alex_jjunio@outlook.com>
2022-12-03 12:15:39 -0300

    create entries and class delete to user

--- gitfav/js/Favorites.js
+++ gitfav/js/Favorites.js
@@ -1,6 +1,34 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+
+    this.entries = [
+      {
+        login: 'alexjjunio',
+        name: 'Alex Junio',
+        public_repos: '76',
+        followers: '1200'
+      },
+      
+      {
+        login: 'maykbrito',
+        name: 'Mayk Brito',
+        public_repos: '86',
+        followers: '1300'
+      }
+    ]
+
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+    this.entries = filteredEntries
+    this.update()
   }
 }
 
@@ -10,21 +38,34 @@ export class FavoritesView extends Favorites {
     this.tbody = this.root.querySelector('table tbody')
     this.update()
   }
-
+  
   update() {
     this.removeAllTr()
-    this.createRow()
     
-    const row = this.createRow()
     
-    this.tbody.append(row)
+    this.entries.forEach(user => {
+      const row = this.createRow()
 
-    row.querySelector('.user img').src = `https://github.com/alexjjunio.png`
-    row.querySelector('.user p').textContent = `Alex Junio`
-    row.querySelector('.user span').textContent = `/alexjjunio`
-    row.querySelector('.repositorios').textContent = `1000`
-    row.querySelector('.followers').textContent = `1000`
-    row.querySelector('.user a').href = `https://github.com/alexjjunio`
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositorios').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
+
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+
+        if(isOk){
+          this.delete(user)
+        }
+      }
+
+      this.tbody.append(row)
+    })
+
+
   }
 
   removeAllTr() {
